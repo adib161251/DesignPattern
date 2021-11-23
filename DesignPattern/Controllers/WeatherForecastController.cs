b39b86dd2e6b400ba14d5d6de9995f80552cdb9c
adib161251 <adib.arman125@gmail.com>
2021-11-23 14:53:16 +0600

    changes

--- DesignPattern/Controllers/WeatherForecastController.cs
+++ DesignPattern/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DesignPattern.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Cold"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
