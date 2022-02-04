0bb8ae086c7010af39d065d171e9e58f730147e8
Syed Adib Arman <adib.arman125@gmail.com>
2022-02-05 02:53:39 +0600

    generic repository pattern integration

---
+++ Services.Interface/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Interface
+{
+    public interface IGenericService<T> where T : class
+    {
+
+    }
+}
---
+++ Services/Services/GenericService.cs
@@ -0,0 +1,11 @@
+﻿using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class GenericService<T>: IGenericService<T> where T : class
+    {
+    }
+}
